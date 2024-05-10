3ff0021ee53ac05a37dbf745717229560ca3723e
Adam Wilson <adamw@ellipticbit.com>
2024-05-09 18:34:45 -0700

    Fix back-offs.

--- Services/Email.SendGrid/SendGridEmailServiceOptions.cs
+++ Services/Email.SendGrid/SendGridEmailServiceOptions.cs
@@ -15,8 +15,8 @@ namespace EllipticBit.Services.Email
 		public string HttpClientName { get; set; } = null;
 		public string Host { get; set; } = null;
 		public int MaxRetries { get; set; } = 3;
-		public TimeSpan MinimumBackOff { get; set; } = TimeSpan.FromSeconds(10);
-		public TimeSpan MaximumBackOff { get; set; } = TimeSpan.FromSeconds(1);
+		public TimeSpan MinimumBackOff { get; set; } = TimeSpan.FromSeconds(1);
+		public TimeSpan MaximumBackOff { get; set; } = TimeSpan.FromSeconds(10);
 		public TimeSpan DeltaBackOff { get; set; } = TimeSpan.FromSeconds(1);
 
 		public SendGridEmailServiceOptions(string apiKey, string fromAddress, string fromName)
