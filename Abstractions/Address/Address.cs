ac20e3cee669908d7b1b1655193d42230aad0f56
Adam Wilson <adamw@ellipticbit.com>
2025-03-09 01:18:33 -0800

    Modify Address class to match new USPS API. Update packages.

--- Abstractions/Address/Address.cs
+++ Abstractions/Address/Address.cs
@@ -1,18 +1,41 @@
-﻿//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // Copyright (c) 2020-2023 EllipticBit, LLC All Rights Reserved.
 //-----------------------------------------------------------------------------
+
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace EllipticBit.Services.Address
 {
 	public sealed class Address
 	{
+
+		[JsonPropertyName("firm")]
+		[DataMember(Name = "firm")] 
 		public string FirmName { get; set; } = string.Empty;
-		public string Address2 { get; set; } = string.Empty;
+
+		[JsonPropertyName("streetAddress")]
+		[DataMember(Name = "streetAddress")]
 		public string Address1 { get; set; } = string.Empty;
-		public string Address3 { get; set; } = string.Empty;
+
+		[JsonPropertyName("secondaryAddress")]
+		[DataMember(Name = "secondaryAddress")]
+		public string Address2 { get; set; } = string.Empty;
+
+		[JsonPropertyName("city")]
+		[DataMember(Name = "city")]
 		public string City { get; set; } = string.Empty;
+
+		[JsonPropertyName("state")]
+		[DataMember(Name = "state")]
 		public string Region { get; set; } = string.Empty;
+
+		[JsonPropertyName("ZIPCode")]
+		[DataMember(Name = "ZIPCode")]
 		public string Zip { get; set; } = string.Empty;
+
+		[JsonPropertyName("ZIPPlus4")]
+		[DataMember(Name = "ZIPPlus4")]
 		public string Zip4 { get; set; } = string.Empty;
 	}
 }
