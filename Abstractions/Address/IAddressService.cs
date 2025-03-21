700a014ce01ae5e1ac3ba0e7e1e0403ee2f55cb0
Adam Wilson <adamw@ellipticbit.com>
2025-03-20 21:28:33 -0700

    Redesign Address interface. Update packages.

--- Abstractions/Address/IAddressService.cs
+++ Abstractions/Address/IAddressService.cs
@@ -1,19 +1,21 @@
-﻿//-----------------------------------------------------------------------------
-// Copyright (c) 2020-2023 EllipticBit, LLC All Rights Reserved.
+//-----------------------------------------------------------------------------
+// Copyright (c) 2020-2025 EllipticBit, LLC All Rights Reserved.
 //-----------------------------------------------------------------------------
 
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace EllipticBit.Services.Address
 {
+	/// <summary>
+	/// Address validation service.
+	/// </summary>
 	public interface IAddressService
 	{
 		/// <summary>
-		/// Provides a list of possible matches to the provided address.
+		/// Provides the closest match to the provided address.
 		/// </summary>
-		/// <param name="address">The Address to validate. May be a partial address.</param>
-		/// <returns>A list of possible addresses matching the provided Address.</returns>
-		Task<List<Address>> Validate(Address address);
+		/// <param name="address">The Address to validate. Can be a partial address.</param>
+		/// <returns>The best match for the provided Address.</returns>
+		Task<Address> Validate(Address address);
 	}
 }
