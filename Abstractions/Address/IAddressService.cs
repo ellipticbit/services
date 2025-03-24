489e2b781b23cec7004ee27ba69768da91f5f2cf
Adam Wilson <adamw@ellipticbit.com>
2025-03-24 02:36:35 -0700

    Add tenant ID.

--- Abstractions/Address/IAddressService.cs
+++ Abstractions/Address/IAddressService.cs
@@ -15,7 +15,8 @@ namespace EllipticBit.Services.Address
 		/// Provides the closest match to the provided address.
 		/// </summary>
 		/// <param name="address">The Address to validate. Can be a partial address.</param>
-		/// <returns>The best match for the provided Address.</returns>
-		Task<Address> Validate(Address address);
+		/// <param name="tenantId">An optional parameter to pass a tenant ID to the underlying authentication system to enable use in multi-tenant systems.</param>
+		/// <returns>The best matching Address for the provided Address.</returns>
+		Task<Address> Validate(Address address, string tenantId = null);
 	}
 }
