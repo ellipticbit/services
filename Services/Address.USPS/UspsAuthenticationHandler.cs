3542237ce5f17cfefbef9a76cdebd7b33c6c91b4
Adam Wilson <adamw@ellipticbit.com>
2025-03-28 02:46:58 -0700

    Rewrite Auth handler to use a Semaphore when updating the token and to avoid the circular reference on Coalesence.

--- Services/Address.USPS/UspsAuthenticationHandler.cs
+++ Services/Address.USPS/UspsAuthenticationHandler.cs
@@ -6,41 +6,60 @@ namespace EllipticBit.Services.Address
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Net.Http;
+	using System.Net.Http.Json;
 	using System.Text.Json.Serialization;
+	using System.Threading;
 	using System.Threading.Tasks;
 	using EllipticBit.Coalescence.Shared;
-	using EllipticBit.Coalescence.Shared.Request;
 
 	internal class UspsAuthenticationHandler : ICoalescenceAuthentication
 	{
-		private readonly ICoalescenceRequestFactory requests;
+		private static SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
+		private readonly IHttpClientFactory factory;
 		private readonly UspsAddressServiceOptions options;
 
-		private string token = string.Empty;
+		private string token = null;
 		private DateTimeOffset expiresAt = DateTimeOffset.MinValue;
 
-		public UspsAuthenticationHandler(ICoalescenceRequestFactory requests, UspsAddressServiceOptions options) {
-			this.requests = requests;
+		public UspsAuthenticationHandler(IHttpClientFactory factory, UspsAddressServiceOptions options) {
+			this.factory = factory;
 			this.options = options;
 		}
 
 		public async Task<string> Get(string tenantId = null) {
-			var credential = await options.GetCredential(tenantId);
-
 			if (expiresAt >= DateTimeOffset.Now) {
 				return token;
 			}
 
-			var tokenResponse = await requests.CreateRequest().Post().Path("oauth2", "v3", "token").FormUrlEncoded(new Dictionary<string, string>() {
-				{ "grant_type", "client_credentials" },
-				{ "client_id", credential.ConsumerKey },
-				{ "client_secret", credential.ConsumerSecret }
-			}).Send();
+			await semaphore.WaitAsync();
+			try {
+				//If we ended up waiting for the semaphore, and it completed, then we already have a new token and should just return it.
+				if (expiresAt >= DateTimeOffset.Now) {
+					return token;
+				}
 
-			var tokenData = await tokenResponse.ThrowOnFailureResponse().AsDeserialized<UspsAuthenticationResponse>();
+				var credential = await options.GetCredential(tenantId);
 
-			expiresAt = DateTimeOffset.FromUnixTimeMilliseconds(tokenData.IssuedAt).AddSeconds(tokenData.ExpiresIn).AddMinutes(-10);
-			token = tokenData.Token;
+				var content = new FormUrlEncodedContent(new Dictionary<string, string>() {
+					{ "grant_type", "client_credentials" },
+					{ "client_id", credential.ConsumerKey },
+					{ "client_secret", credential.ConsumerSecret }
+				});
+
+				using var http = factory.CreateClient("USPS");
+				using var response = await http.PostAsync(new Uri("oauth2/v3/token", UriKind.Relative), content);
+				response.EnsureSuccessStatusCode();
+				var tokenData = await response.Content.ReadFromJsonAsync<UspsAuthenticationResponse>();
+
+				token = tokenData.Token;
+				expiresAt = DateTimeOffset.FromUnixTimeMilliseconds(tokenData.IssuedAt).AddSeconds(tokenData.ExpiresIn).AddMinutes(-10);
+			}
+			finally
+			{
+				semaphore.Release();
+			}
 
 			return token;
 		}
