9b14b0957601b4d7d387477ef9c81aad856ffab2
Adam Wilson <adamw@ellipticbit.com>
2025-06-19 22:47:09 -0700

    Fix SendGridTemplate attachments.

--- Services/Email.SendGrid/SendGridTemplate.cs
+++ Services/Email.SendGrid/SendGridTemplate.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------------
-// Copyright (c) 2024 EllipticBit, LLC All Rights Reserved.
+// Copyright (c) 2024-2025 EllipticBit, LLC All Rights Reserved.
 //-----------------------------------------------------------------------------
 
 using System;
@@ -13,7 +13,7 @@ namespace EllipticBit.Services.Email
 		public int? UnsubscribeGroupId { get; set; } = null;
 		public IEnumerable<int> UnsubscribeGroups { get; set; } = null;
 		public DateTimeOffset? SendAt { get; set; } = null;
-		public IEnumerable<EmailAttachment> Attachments { get; set; } = null;
+		public IEnumerable<IEmailAttachment> Attachments { get; set; } = null;
 		public object TemplateData { get; set; } = null;
 
 		public SendGridTemplate(EmailAddress toAddress, string templateId) : base(toAddress) {
